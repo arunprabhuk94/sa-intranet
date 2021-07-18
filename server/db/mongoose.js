9d62073c392151339601d1c7327b508ab7b784df
Arun Prabhu K <arunprabhuk94@gmail.com>
2021-07-18 18:52:14 +0530

    Add docker-compose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
+const mongoUser = process.env.MONGODB_USERNAME || "127.0.0.1";
+const mongoPwd = process.env.MONGODB_PASSWORD || "127.0.0.1";
+const mongoHost = process.env.MONGODB_HOST || "127.0.0.1";
+const mongoPort = process.env.MONGODB_PORT || "27017";
+const mongoDbName = process.env.MONGODB_DB || "workspace-management";
 const mongoUrl =
-  process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/workspace-management";
+  process.env.MONGODB_URL ||
+  `mongodb:${mongoUser}:${mongoPwd}//${mongoHost}:${mongoPort}/${mongoDbName}`;
 
 const connectMongoDb = async () => {
   try {
